eb7cd0bb07249c347f874be7fd42b0abf4d868f5
knk4748 <kanishk9898@gmail.com>
2020-08-21 17:59:59 +0530

    init

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+$(".btn1").click(  function(){
+    $(".x").toggleClass("hide");
+    $(".x").toggleClass("show");
+    console.log("clicked");
+} );
+$(".btn2").click(  function(){
+    $(".y").toggleClass("hide");
+    $(".y").toggleClass("show");
+    console.log("clicked");
+} );
+$(".btn3").click(  function(){
+    $(".z").toggleClass("hide");
+    $(".z").toggleClass("show");
+    console.log("clicked");
+} );
